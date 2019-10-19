c572b48692f98b739ce46e02d0c3172a732d6dd3
Pirori3182 <pomuo.0331@gmail.com>
2019-10-19 18:49:28 +0900

    users_controllerにeditとupdateアクションを追加

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,10 @@
+class UsersController < ApplicationController
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    user = User.find(params[:id])
+    user.update(user_params) if user_id == current_user.id
+  end
+end
