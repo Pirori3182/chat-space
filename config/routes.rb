025a3370e335a7c06a06b38842cc0ee70e3a3a46
Pirori3182 <pomuo.0331@gmail.com>
2019-10-20 17:29:57 +0900

    make group controller and group model

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
+  get 'groups/new'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'messages#index'
   resources :users, only: [:edit, :update]
 end
