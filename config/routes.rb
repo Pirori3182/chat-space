1d7d9126636593b5d4c3c238cb6430b94e9e17cb
Pirori3182 <pomuo.0331@gmail.com>
2019-10-22 15:01:39 +0900

    groupsルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'groups/new'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'messages#index'
   resources :users, only: [:edit, :update]
+  resources :groups, only: [:new, :edit, :create, :update]
 end
