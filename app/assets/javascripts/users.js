c5b46b53a86228e943c31d4400b5a49ab379ee88
Pirori3182 <pomuo.0331@gmail.com>
2019-11-23 17:20:22 +0900

    インクリメンタルサーチ_191123

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -6,7 +6,7 @@ $(function() {
       <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${ user.id }" data-user-name="${ user.name }">追加</div>
     </div>
     `;
-    $("#user-search-result").append(html);  //なんのため？
+    $("#user-search-result").append(html);  //上のHTMLデータを出力するため
   }
   function addNoUser(){ // 引数いらないんだ
     let html =  `
@@ -15,6 +15,18 @@ $(function() {
         </div>
     `;
     $("#user-search-result").append(html);
+  }
+  function addDeleteUser(name, id) {
+    let html = `
+    <div class="chat-group-user clearfix" id="${id}">
+      <p class="chat-group-user__name">${name}</p>
+      <div class="user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn" data-user-id="${id}" data-user-name="${name}">削除</div>
+    </div>`;
+    $(".js-add-user").append(html);
+  }
+  function addMember(userId) {
+    let html = `<input value="${userId}" name="group[user_ids][]" type="hidden" id="group_user_ids_${userId}" />`;
+    $(`#${userId}`).append(html);
   }
   
   $('#user-search-field').on('keyup' , function(){
@@ -39,11 +51,24 @@ $(function() {
         }
       })
       .fail(function(){
-        alert("通信エラーです。ユーザーが表示できません。");
+        // alert("通信エラーです。ユーザーが表示できません。");
+        console.log("失敗")
       });
   });
+  
+  $(document).on("click", ".chat-group-user__btn--add", function() { // $(document).on('click', '【セレクタ】', function ()で、常に最新のHTMLの情報を取得することができる
+    const userName = $(this).attr("data-user-name");
+    const userId = $(this).attr("data-user-id");
+    $(this)
+      .parent()
+      .remove();
+    addDeleteUser(userName, userId);
+    addMember(userId);
+  });
+  $(document).on("click", ".chat-group-user__btn--remove", function() {
+    $(this)
+      .parent()
+      .remove();
+  });
 
-  $(".user-search-add chat-group-user__btn chat-group-user__btn--add").on('click' , function(){
-    console.log("追加")
-  }
 });
